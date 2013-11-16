580821ae5d17fb7a9f2bad652b8a0e6443e2522a
Chuz Lin <shmcclmm@gmail.com>
2013-11-16 20:21:49 +0800

    feature_whitedot: add base view and base funcs

--- src/com/lcz/screenlock/WhiteDot.java
+++ src/com/lcz/screenlock/WhiteDot.java
@@ -2,94 +2,207 @@ package com.lcz.screenlock;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
 
 public class WhiteDot {
-	private Button btn_floatView;
+	
+	
+	static boolean DBG = true;
+	static String TAG = "ScreenLock-WhiteDot";
+	
+
+	Context mContext;
+	LayoutInflater mLayoutInflater;
 	private WindowManager wm;
-	private LayoutParams params;
-	private boolean isAdded = false; // 是否已增加悬浮窗  
+	ControlBoard mControlBoard;
+	
+	private boolean floatbtn_isAdded = false; // 是否已增加悬浮按钮
+	private Button float_btn;
+	private Drawable float_btn_bg_drawable;
+	private LayoutParams params_btn;
+	private int isClick = 0;
+	
+	
+	private static final int MSG_BASE = 0;
+	private static final int MSG_SHOW_FLOATBTN = MSG_BASE + 1;
+	private static final int MSG_HIDE_FLOATBTN = MSG_BASE + 2;
+	
+	
+	Handler mHandler = new Handler(){
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case MSG_SHOW_FLOATBTN:
+//				float_btn.setVisibility(View.VISIBLE);
+				mControlBoard.setVisibility(View.GONE);
+				break;
+			case MSG_HIDE_FLOATBTN:
+//				float_btn.setVisibility(View.GONE);
+				mControlBoard.setVisibility(View.VISIBLE);
+				break;
+
+			default:
+				break;
+			}
+		};
+	};
+	
 	
 	public WhiteDot() {
 		// TODO Auto-generated constructor stub
 		this(null);
 	}
-	
+
 	public WhiteDot(Context context) {
 		// TODO Auto-generated constructor stub
-		createFloatView(context);
+		mContext = context;
+		mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		wm = (WindowManager) mContext.getApplicationContext().getSystemService(
+				Context.WINDOW_SERVICE);
+		createFloatBtn(mContext);
+		mControlBoard = new ControlBoard(mContext,new ControlBoard.ControlBoard_Interface() {
+			
+			@Override
+			public void setVisibility(int visibility) {
+				// TODO Auto-generated method stub
+				if(visibility == View.GONE || visibility == View.INVISIBLE){
+					float_btn.setVisibility(View.VISIBLE);
+				}else if(visibility == View.VISIBLE){
+					float_btn.setVisibility(View.INVISIBLE);
+				}
+			}
+		});
+		mControlBoard.setVisibility(View.GONE);
 	}
-	
+
 	/**
-	 * 创建悬浮窗
+	 * 创建悬浮按钮
 	 */
-	private void createFloatView(Context context) {
-		if (isAdded) {
+	private void createFloatBtn(Context context) {
+		if (floatbtn_isAdded) {
 			return;
 		}
-		btn_floatView = new Button(context.getApplicationContext());
-        btn_floatView.setText("悬浮窗");
-        
-        wm = (WindowManager) context.getApplicationContext()
-        	.getSystemService(Context.WINDOW_SERVICE);
-        params = new WindowManager.LayoutParams();
-        
-        // 设置window type
-        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-        /*
-         * 如果设置为params.type = WindowManager.LayoutParams.TYPE_PHONE;
-         * 那么优先级会降低一些, 即拉下通知栏不可见
-         */
-        
-        params.format = PixelFormat.RGBA_8888; // 设置图片格式，效果为背景透明
-        
-        // 设置Window flag
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                              | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        /*
-         * 下面的flags属性的效果形同“锁定”。
-         * 悬浮窗不可触摸，不接受任何事件,同时不影响后面的事件响应。
-        wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL
-                               | LayoutParams.FLAG_NOT_FOCUSABLE
-                               | LayoutParams.FLAG_NOT_TOUCHABLE;
-         */
-        
-        // 设置悬浮窗的长得宽
-        params.width = 100;
-        params.height = 100;
-        
-        // 设置悬浮窗的Touch监听
-        btn_floatView.setOnTouchListener(new OnTouchListener() {
-        	int lastX, lastY;
-        	int paramX, paramY;
-        	
+		float_btn = new Button(context.getApplicationContext());
+		float_btn.setBackgroundResource(R.drawable.float_btn_bg_iphone);
+		float_btn_bg_drawable = float_btn.getBackground();
+		
+		params_btn = new WindowManager.LayoutParams();
+
+		// 设置window type
+		params_btn.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+		/*
+		 * 如果设置为params.type = WindowManager.LayoutParams.TYPE_PHONE; 那么优先级会降低一些,
+		 * 即拉下通知栏不可见
+		 */
+
+		params_btn.format = PixelFormat.RGBA_8888; // 设置图片格式，效果为背景透明
+
+		// 设置Window flag
+		params_btn.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+				| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+		/*
+		 * 下面的flags属性的效果形同“锁定”。 悬浮按钮不可触摸，不接受任何事件,同时不影响后面的事件响应。
+		 * wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL |
+		 * LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_NOT_TOUCHABLE;
+		 */
+
+		// 设置悬浮按钮的长得宽
+		params_btn.width = 100;
+		params_btn.height = 100;
+
+		// 设置悬浮按钮的Touch监听
+		float_btn.setOnTouchListener(new OnTouchListener() {
+			int lastX, lastY;
+			int paramX, paramY;
+
 			public boolean onTouch(View v, MotionEvent event) {
-				switch(event.getAction()) {
+				switch (event.getAction()) {
 				case MotionEvent.ACTION_DOWN:
 					lastX = (int) event.getRawX();
 					lastY = (int) event.getRawY();
-					paramX = params.x;
-					paramY = params.y;
+					paramX = params_btn.x;
+					paramY = params_btn.y;
+					isClick = 0;
+					if(DBG)Log.d(TAG, "ACTION_DOWN");
 					break;
 				case MotionEvent.ACTION_MOVE:
 					int dx = (int) event.getRawX() - lastX;
 					int dy = (int) event.getRawY() - lastY;
-					params.x = paramX + dx;
-					params.y = paramY + dy;
-					// 更新悬浮窗位置
-			        wm.updateViewLayout(btn_floatView, params);
+					params_btn.x = paramX + dx;
+					params_btn.y = paramY + dy;
+					// 更新悬浮按钮位置
+					wm.updateViewLayout(float_btn, params_btn);
+					isClick++;
+					if(DBG)Log.d(TAG, "ACTION_MOVE");
+					break;
+				case MotionEvent.ACTION_UP:
+					if(DBG)Log.d(TAG, "ACTION_UP");
 					break;
 				}
-				return true;
+				return false;//true 其他的动作都捕捉不到，比如Click，longClick等等
 			}
 		});
-        
-        wm.addView(btn_floatView, params);
-        isAdded = true;
+		
+		/*
+		 * OnClickListener只发生在up之后
+		 */
+		float_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if(DBG)Log.d(TAG, "onClick");
+				if(isClick < 5){
+					mHandler.sendEmptyMessage(MSG_HIDE_FLOATBTN);
+				}
+			}
+		});
+		
+		/*
+		 * OnLongClickListene可能发生在down-move LongClick move-up
+		 */
+		float_btn.setOnLongClickListener(new OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				// TODO Auto-generated method stub
+				if(DBG)Log.d(TAG, "onLongClick");
+				return false;
+			}
+		});
+		
+		wm.addView(float_btn, params_btn);
+		floatbtn_isAdded = true;
+	}
+	
+	
+	/**
+	 * @author lcz
+	 * @param resid
+	 */
+	void setFloatBtn_BackgroundResource(int resid){
+		if(float_btn != null){
+			float_btn.setBackgroundResource(resid);
+		}
+	}
+	
+	/**
+	 * @author lcz
+	 * @param alpha [0-255]
+	 */
+	void setFloatBtn_bg_alpha(int alpha){
+		if(float_btn_bg_drawable != null){
+			float_btn_bg_drawable.setAlpha(alpha);
+		}
 	}
 }
